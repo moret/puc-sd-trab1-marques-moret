f03f625c5455371ac0b7990d882d9031e2eb4b44
google@moret.pro.br <google@moret.pro.br@d7ba3d01-b80f-1679-f432-6a4b9edec859>
2010-04-07 01:14:31 +0000

    Logs para testes mono 100KB e 1MB; Problemas de conexão, os testes de 100KB precisam ser refeitos

---
+++ run-all-client-tests.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+echo 15-1
+lua -e "simultaneous_clients = 15" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli15-files20-run1.log
+echo 15-2
+lua -e "simultaneous_clients = 15" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli15-files20-run2.log
+echo 15-3
+lua -e "simultaneous_clients = 15" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli15-files20-run3.log
+
+echo 10-1
+lua -e "simultaneous_clients = 10" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli10-files20-run1.log
+echo 10-2
+lua -e "simultaneous_clients = 10" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli10-files20-run2.log
+echo 10-3
+lua -e "simultaneous_clients = 10" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli10-files20-run3.log
+
+echo 5-1
+lua -e "simultaneous_clients = 5" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli5-files20-run1.log
+echo 5-2
+lua -e "simultaneous_clients = 5" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli5-files20-run2.log
+echo 5-3
+lua -e "simultaneous_clients = 5" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli5-files20-run3.log
+
+echo 1-1
+lua -e "simultaneous_clients = 1" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli1-files20-run1.log
+echo 1-2
+lua -e "simultaneous_clients = 1" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli1-files20-run2.log
+echo 1-3
+lua -e "simultaneous_clients = 1" -e "serv_addr = '10.0.2.18'" multi_client.lua > moret-servmono-cli1-files20-run3.log
+
+
+
